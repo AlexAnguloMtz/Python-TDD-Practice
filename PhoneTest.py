07d153cec30fe466ec68c2bff2dab3b0c6edc312
alex <raulalexo100@gmail.com>
2023-01-25 10:24:09 -0700

    Added a unit test

--- PhoneTest.py
+++ PhoneTest.py
@@ -14,6 +14,9 @@ class PhoneTest(unittest.TestCase):
     def test_should_parse_phone_extension(self):
         phone = Phone('+52-6622778899-78')
         self.assertEqual('78', phone.extension)
+
+    def test_should_raise_exception_for_invalid_phone_format(self):
+        self.assertRaises(Exception, lambda: Phone('2-45-89'))
         
 if __name__ == '__main__':
     unittest.main()

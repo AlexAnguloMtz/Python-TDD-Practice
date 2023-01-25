a71fd20704c328aaba059289c7ec23d73e7ff3a9
alex <raulalexo100@gmail.com>
2023-01-24 17:12:47 -0700

    Finished

--- excercise-three.py
+++ excercise-three.py
@@ -2,12 +2,13 @@
 Consider a phone with the following format: prefix-number-extension. 
 The prefix is the country's code and the extension
 has two digits. For example: +52-6621012345-56 
-Escribir un programa que pregunte por un número 
-de teléfono con este formato en la consola y muestre
-por pantalla el número de teléfono sin el prefijo y la extensión
+Write a program to ask the user for a phone number
+with this format and show the phone with no prefix or extension
 """
 
 def main():
-
+    formatted_phone = input('Enter your phone number: ')
+    phone = formatted_phone.split('-')[1]
+    print(f'Your phone number is: {phone}')
 
 main()
